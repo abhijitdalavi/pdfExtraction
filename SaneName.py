6da3f9a7af5e4a1f71288b10b1421bbbe6031eb8
mooncrater31 <34881801+mooncrater31@users.noreply.github.com>
2018-07-28 17:21:19 +0530

    Completed. Add documentation.

--- SaneName.py
+++ SaneName.py
@@ -1,3 +1,4 @@
+#@numba.jit
 def nameStops(name):
     dots = findOccurrences(name,'.')
     dotNM = [True]*len(dots)
@@ -21,33 +22,6 @@ def nameStops(name):
                             dotNM[i] = False 
                             break
     return name,dotNM             
-    '''(' bI.', 'B.'),
- (' sI.', 'C.'),
- (' DI.', 'D.'),
- (' epha.', 'F.'),
- (' jI.', 'G.'),
- (' echa.', 'H.'),
- (' ke.', 'K.'),
- (' ela.', 'L.'),
- (' ema.', 'M.'),
- (' em.','M.'),        
- (' ena.', 'N.'),
- (' o.', 'O.'),
- (' pI.', 'P.'),
- (' Ara.', 'R.'),
- (' esa.', 'S.'),
- (' TI.', 'T.'),
- (' yU.', 'U.'),
- (' vI.', 'V.'),
- (' DablyU.', 'W.'),
- (' eksa.', 'X.'),
- (' vAI.', 'Y.'),  
- (' jeDa.', 'Z.'),
- (' kyU.', 'Q.'),    
- (' je.', 'J.'),    
- (' AI.', 'I.'),
- (' I.', 'E.'),
- (' e.', 'A.'),'''
 mapping = [('bI.', 'B.'),
  ('sI.', 'C.'),
  ('DI.', 'D.'),
@@ -107,6 +81,7 @@ mapping = [('bI.', 'B.'),
  ('e ', 'A. ')
           
           ]    
+#@numba.jit
 def muhammadification(name):
     mohds = ['mo.', 'moha.', 'mohamada.', 'mohammada.']
     for i in mohds:
@@ -117,6 +92,7 @@ def muhammadification(name):
             else:
                 name = name.replace(i,'Muhammad')
     return name
+#@numba.jit
 def dealWithDOT(name):
     name = muhammadification(name)
     name,dotNM = nameStops(name)
@@ -125,5 +101,87 @@ def dealWithDOT(name):
         if val:
             name = name[:dots[i]]+name[dots[i]+1:]
     return name
+#@numba.jit
 def findOccurrences(s, ch):# taken from: https://stackoverflow.com/a/13009866/5345646
     return [i for i, letter in enumerate(s) if letter == ch]
+#@numba.jit
+def dealWithTilde(name):
+    if '~' in name:
+        name = name.replace('j~n','gy')
+    return name 
+#@numba.jit
+def dealWithChandra(name):
+    if 'ॉ' in name:
+        name = name.replace('ॉ','u')
+    return name
+#@numba.jit
+def dealWithZWJ(name):
+    if '\u200d' in name:
+        name = name.replace('\u200d','')
+    return name
+#@numba.jit
+def dealWithNuqta(name):
+    if '़' in name:
+        name = name.replace('़','')
+    return name
+#@numba.jit
+def dealWithAnudatta(name):
+    if '॒' in name:
+        name = name.replace('a'+chr(2386),'')
+    return name
+#@numba.jit
+def dealWithZWNJ(name):
+    if '\u200d' in name:
+        name = name.replace('\u200d','')
+    return name  
+#@numba.jit
+
+def nameMagic(df):
+    names = np.copy(df['Name'].values)
+    delLst,col = nameMagicCol(names)
+    dfNew = df.copy(deep=True)
+    dfNew['Name'] = col
+    #print(':::: '+str(len(dfNew)))
+    #print(str(delLst))
+    dfNew = dfNew.drop(dfNew.index[delLst]) #Why is this not working?
+    #print(':::: '+str(len(dfNew)))
+    dfNew = dfNew.reset_index(drop=True)
+    return dfNew
+# 42s no-numba   
+# numpy 1.54 s
+def tester(name):
+    
+    name = dealWithDOT(name)
+    name = dealWithZWJ(name)
+    #print('Name after ZWJ:'+name)
+    name = dealWithChandra(name)
+    name = dealWithNuqta(name)
+    name = dealWithZWNJ(name)
+    name = dealWithAnudatta(name)
+    name = dealWithTilde(name)
+    noSpaces = name.replace(' ','') 
+    noSpaces = noSpaces.replace('.','')
+    return name 
+def nameMagicCol(col):
+    delLst = []
+    for i in range(len(col)):
+        name = col[i]
+        name = dealWithDOT(name)
+        name = dealWithZWJ(name)
+        #print('Name after ZWJ:'+name)
+        name = dealWithChandra(name)
+        name = dealWithNuqta(name)
+        name = dealWithZWNJ(name)
+        name = dealWithAnudatta(name)
+        name = dealWithTilde(name)
+        noSpaces = name.replace(' ','') 
+        noSpaces = noSpaces.replace('.','')
+        #print('noSpaces :'+noSpaces)
+        if not noSpaces.isalpha():
+            delLst.append(i)
+            print('removing '+str(i))
+        else:
+            col[i] = name   
+    return delLst,col           
+
+    
